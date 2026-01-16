e077db7e53de1a6d3c715db3e92298ed02bff64d
eL <elvisiondragon@gmail.com>
2026-01-16 23:22:29 +0700

    DREF NO DISCONECT

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Product from "./pages/Product";
-import Why from "./pages/Why";
-import Meditate from "./pages/Meditate";
-import Checkout from "./pages/Checkout";
-import NotFound from "./pages/NotFound";
-import { MobileNavigation } from "@/components/MobileNavigation";
+import DrelfLanding from "./pages/drelflp"; 
 import Pixel from "./pages/Pixel";
 import FloatingWhatsAppButton from "@/components/FloatingWhatsAppButton";
 
@@ -24,15 +18,8 @@ const App = () => (
         <Pixel /> 
         <div className="relative">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/product" element={<Product />} />
-            <Route path="/why" element={<Why />} />
-            <Route path="/meditate" element={<Meditate />} />
-            <Route path="/checkout" element={<Checkout />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
+            <Route path="/" element={<DrelfLanding />} /> 
           </Routes>
-          <MobileNavigation />
           <FloatingWhatsAppButton />
         </div>
       </BrowserRouter>
